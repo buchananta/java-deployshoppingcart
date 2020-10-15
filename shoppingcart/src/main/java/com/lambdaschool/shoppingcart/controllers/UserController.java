db4ebbbfa3ddd6d946bb639ddb57b0c2d339fb53
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-10-15 15:38:53 -0500

    copied over shoppingcart project

---
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/controllers/UserController.java
@@ -0,0 +1,89 @@
+package com.lambdaschool.shoppingcart.controllers;
+
+import com.lambdaschool.shoppingcart.models.User;
+import com.lambdaschool.shoppingcart.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController
+{
+    @Autowired
+    private UserService userService;
+
+    @PreAuthorize(value = "hasAnyRole('ADMIN')")
+    @GetMapping(value = "/users", produces = {"application/json"})
+    public ResponseEntity<?> listAllUsers()
+    {
+        List<User> myUsers = userService.findAll();
+        return new ResponseEntity<>(myUsers, HttpStatus.OK);
+    }
+
+    @PreAuthorize(value = "hasAnyRole('ADMIN')")
+    @GetMapping(value = "/user/{userId}",
+            produces = {"application/json"})
+    public ResponseEntity<?> getUserById(
+            @PathVariable
+                    Long userId)
+    {
+        User u = userService.findUserById(userId);
+        return new ResponseEntity<>(u,
+                                    HttpStatus.OK);
+    }
+
+    @PreAuthorize(value = "hasAnyRole('ADMIN')")
+    @PostMapping(value = "/user", consumes = {"application/json"})
+    public ResponseEntity<?> addUser(@Valid @RequestBody User newuser)
+    {
+        newuser.setUserid(0);
+        newuser = userService.save(newuser);
+
+        // set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newUserURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{userid}")
+                .buildAndExpand(newuser.getUserid())
+                .toUri();
+        responseHeaders.setLocation(newUserURI);
+
+        return new ResponseEntity<>(null,
+                                    responseHeaders,
+                                    HttpStatus.CREATED);
+    }
+    @PreAuthorize(value = "hasAnyRole('ADMIN')")
+    @DeleteMapping(value = "/user/{userId}")
+    public ResponseEntity<?> deleteUserById(
+            @PathVariable
+                    Long userId)
+    {
+        userService.delete(userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/myinfo", produces = {"application/json"})
+    public ResponseEntity<?> getCurrentUserInfo()
+    {
+
+        String uname = SecurityContextHolder.getContext().getAuthentication().getName();
+        User u = userService.findUserByName(uname);
+        return new ResponseEntity<>(u, HttpStatus.OK);
+    }
+}
